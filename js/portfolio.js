1304e6175858c4ff4f557c65ca3e670fc501ad35
mmckeaveney <martinmckeaveney@gmail.com>
2015-08-31 18:04:13 +0100

    added smaller images and portfolio piece

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+
+$(".fancybox").fancybox({
+        'transitionIn'  :   'elastic',
+        'transitionOut' :   'elastic',
+        'speedIn'       :   600, 
+        'speedOut'      :   200, 
+        'overlayShow'   :   false,
+        'showNavArrows' :   true,
+        showArrows  : true,
+        arrows      : true,
+
+    });
